be506cf010fe2cce304e38e783ced3f12b7cf291
KhooLayHan <khoolayhan@github.com>
2026-07-10 20:32:47 +0800

    fix: remove duplicated configuration with web.ts

--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -1,17 +1,12 @@
 import { createEnv } from "@t3-oss/env-core";
-import { z } from "zod";
 
 export const env = createEnv({
   clientPrefix: "PUBLIC_",
   client: {
-    PUBLIC_SENTRY_DSN: z.httpUrl(),
   },
   server: {
-    SENTRY_SERVER_DSN: z.httpUrl(),
   },
   runtimeEnv: {
-    PUBLIC_SENTRY_DSN: process.env.PUBLIC_SENTRY_DSN,
-    SENTRY_SERVER_DSN: process.env.SENTRY_SERVER_DSN,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,
