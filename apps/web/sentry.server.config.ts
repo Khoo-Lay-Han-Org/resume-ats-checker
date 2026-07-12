471d9064a095cdbbce2ec0882cd32605099112ce
KhooLayHan <khoolayhan@github.com>
2026-07-12 09:15:51 +0800

    fix: remove duplicate config

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -2,8 +2,8 @@ import * as Sentry from "@sentry/cloudflare";
 import handler from "@astrojs/cloudflare/entrypoints/server";
 
 export default Sentry.withSentry(
-  (_env) => ({
-    dsn: import.meta.env.SENTRY_SERVER_DSN,
+  (env) => ({
+    dsn: env.SENTRY_SERVER_DSN,
     dataCollection: {
       // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
       // https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#dataCollection
