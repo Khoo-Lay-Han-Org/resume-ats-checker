5a321d19ee991c892ac4a8dca36a9be09f1a5f11
includecctype <zhanthong@gmail.com>
2026-08-12 00:17:36 +0800

    feat: restore vectors

--- apps/ai-ml/train/job_type_model/rag_dataset/helper_func_for_backup/embed.py
+++ apps/ai-ml/train/job_type_model/rag_dataset/helper_func_for_backup/embed.py
@@ -1,2 +1,16 @@
+from ..service.embedding_model import model
+from ..storage.route_config import STORED_DATA
+import json
+
+
 def embed_recorded_content():
-    pass
+    with open(STORED_DATA, "r") as f:
+        data = json.load(f)
+
+    all_content = []
+    for item in data:
+        result = model.encode(item, truncate_dim=50, normalize_embeddings=True)
+
+        all_content.append({"text": item, "vector": result})
+
+    return all_content
--- apps/ai-ml/train/job_type_model/rag_dataset/helper_func_for_backup/store.py
+++
@@ -1,2 +0,0 @@
-def store_recorded_content():
-    pass
