1da61430fc1c58a2bfd6855ac4300f9930ad2959
includecctype <zhanthong@gmail.com>
2026-08-11 22:46:51 +0800

    feat: recording progress

--- apps/ai-ml/train/job_type_model/rag_dataset/helper_func_for_search/progress_record.py
+++ apps/ai-ml/train/job_type_model/rag_dataset/helper_func_for_search/progress_record.py
@@ -1,25 +1,44 @@
 import json
+from ..storage.route_config import CHECKPOINT_FILE, STORED_DATA
 
 
 def files_creation_for_record():
-    with open("../storage/checkpoint.json", "w") as f:
+    with open(CHECKPOINT_FILE, "w") as f:
         json.dump([], f)
-    with open("../storage/stored_data.json", "w") as f:
+    with open(STORED_DATA, "w") as f:
         json.dump([], f)
 
 
 def record_ideated_labels(queries):
-    with open("../storage/checkpoint.json", "r") as f:
+    new_data = {"label": queries, "index": 0}
+
+    with open(CHECKPOINT_FILE, "r") as f:
         data = json.load(f)
 
+    if not isinstance(data, list):
+        raise Exception("data is not list")
 
-## Use this everytime done recording polished content
+    data.append(new_data)
+
+    with open(CHECKPOINT_FILE, "w") as f:
+        json.dump(data, f)
+
+
 def record_label_indexing():
-    pass
+    with open(CHECKPOINT_FILE, "r") as f:
+        data = json.load(f)
+
+    if not isinstance(data, list):
+        raise Exception("data is not list")
+
+    data[-1]["index"] += 1
+
+    with open(CHECKPOINT_FILE, "w") as f:
+        json.dump(data, f)
 
 
 def record_each_polished_content(queries):
-    with open("../storage/stored_data.json", "r") as f:
+    with open(STORED_DATA, "r") as f:
         data = json.load(f)
 
     if not isinstance(data, list):
@@ -28,5 +47,5 @@ def record_each_polished_content(queries):
     for query in queries:
         data.append(query)
 
-    with open("../storage/stored_data.json", "w") as f:
+    with open(STORED_DATA, "w") as f:
         json.dump(data, f)
---
+++ apps/ai-ml/train/job_type_model/rag_dataset/storage/route_config.py
@@ -0,0 +1,5 @@
+import os
+
+CHECKPOINT_FILE = os.path.dirname(os.path.abspath(__file__)) + "/checkpoint.json"
+STORED_DATA = os.path.dirname(os.path.abspath(__file__)) + "/stored_data.json"
+VECTOR_DB_STORE = os.path.dirname(os.path.abspath(__file__)) + "/database.db"
